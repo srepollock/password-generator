9636a7c9e0625ce042b42ba25f3cb191fee9009f
Spencer Pollock <spencer@spollock.ca>
2018-07-13 01:51:10 -0700

    :zzz: Initial application. Not yet complete.

---
+++ Generate App/SlideViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SlideViewController.swift
+//  Generate App
+//
+//  Created by Spencer Pollock on 2018-07-13.
+//  Copyright © 2018 spollock. All rights reserved.
+//
+
+import UIKit
+
+public class SlideViewController: UIViewController {
+    
+}
